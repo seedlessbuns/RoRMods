2c32d3da1b31b48fea77d03ff71dd25294a847e9
seedlessbuns <50305369+seedlessbuns@users.noreply.github.com>
2019-05-16 14:00:10 -0400

    Add files via upload

--- Legacy Infusion/legacyInfusion.cs
+++ Legacy Infusion/legacyInfusion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BepInEx;
+using BepInEx.Configuration;
 using RoR2;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -7,18 +8,32 @@ using UnityEngine.Networking;
 namespace legacyInfusion
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.seedlessbuns.legacyInfusion", "Legacy Infusion", "1.0.0")]
-    public class MyModName : BaseUnityPlugin
+    [BepInPlugin("com.seedlessbuns.legacyInfusion", "Legacy Infusion", "1.1.0")]
+    public class legacyInfusion : BaseUnityPlugin
     {
+
+        private static ConfigWrapper<int> capConfig { get; set; }    
+
+        public static int cap
+        {
+            get => capConfig.Value;
+            protected set => capConfig.Value = value;
+        }
+
+
+
         public void Awake()
         {
 
+            capConfig = Config.Wrap("Settings", "Health limit", "Sets the health limit per infusion (0 = no limit, minimum is 100)", 0);
+            cap = capConfig.Value;
+            //Chat.AddMessage("Cap: " + cap);
 
             On.RoR2.Inventory.AddInfusionBonus += (orig, self, value) =>
             {
 
                 value *= (uint)self.GetItemCount(ItemIndex.Infusion);
-
+                //Chat.AddMessage("InfusionBOnus: " + self.infusionBonus);
 
                 orig(self, value);
 
@@ -43,14 +58,32 @@ namespace legacyInfusion
                             if(infusionCount > 0)
                             {
                                 int maxHp = infusionCount * 100;
-                                if((ulong)inventory.infusionBonus >= (ulong)((long)maxHp))
+                                if (cap != 0)
                                 {
-                                    RoR2.Orbs.InfusionOrb infusionOrb = new RoR2.Orbs.InfusionOrb();
-                                    infusionOrb.origin = gameObject.transform.position;
-                                    infusionOrb.target = Util.FindBodyMainHurtBox(player);
-                                    infusionOrb.maxHpValue = 1;
-                                    RoR2.Orbs.OrbManager.instance.AddOrb(infusionOrb);
+                                    if ((ulong)inventory.infusionBonus >= (ulong)((long)maxHp) && inventory.infusionBonus < infusionCount * cap)
+                                    {
+                                        RoR2.Orbs.InfusionOrb infusionOrb = new RoR2.Orbs.InfusionOrb();
+                                        infusionOrb.origin = gameObject.transform.position;
+                                        infusionOrb.target = Util.FindBodyMainHurtBox(player);
+                                        infusionOrb.maxHpValue = 1;
+                                        RoR2.Orbs.OrbManager.instance.AddOrb(infusionOrb);
+                                        //Chat.AddMessage("not at cap " + cap + " yet. Have " + inventory.infusionBonus + " hp");
 
+                                    } //if (inventory.infusionBonus >= infusionCount * cap) { Chat.AddMessage("at cap"); }
+                                }
+                                else
+                                {
+
+                                    if ((ulong)inventory.infusionBonus >= (ulong)((long)maxHp))
+                                    {
+                                        RoR2.Orbs.InfusionOrb infusionOrb = new RoR2.Orbs.InfusionOrb();
+                                        infusionOrb.origin = gameObject.transform.position;
+                                        infusionOrb.target = Util.FindBodyMainHurtBox(player);
+                                        infusionOrb.maxHpValue = 1;
+                                        RoR2.Orbs.OrbManager.instance.AddOrb(infusionOrb);
+                                        //Chat.AddMessage("no cap: ");
+
+                                    }
                                 }
                             }
                         }
@@ -60,6 +93,27 @@ namespace legacyInfusion
             };
 
         }
+
+
+        /*public void Update()
+        {
+            //This if statement checks if the player has currently pressed F2, and then proceeds into the statement:
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                //We grab a list of all available Tier 3 drops:
+                var dropList = Run.instance.availableTier2DropList;
+
+                //Randomly get the next item:
+                var nextItem = Run.instance.treasureRng.RangeInt(0, dropList.Count);
+
+                //Get the player body to use a position:
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+
+                //And then finally drop it infront of the player.
+                PickupDropletController.CreatePickupDroplet(new RoR2.PickupIndex(ItemIndex.Infusion), transform.position, transform.forward * 20f);
+            }
+        }*/
+
 
     }
 }
